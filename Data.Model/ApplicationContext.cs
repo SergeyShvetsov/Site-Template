e04bdd2b59a4b38218984086d682ac7b2ffe7742
SergeyShvetsov <svs1966uz@gmail.com>
2020-06-11 03:32:43 +0500

    Initialize

---
+++ Data.Model/ApplicationContext.cs
@@ -0,0 +1,18 @@
+﻿using Data.Model.Models;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace Data.Model
+{
+    public class ApplicationContext : IdentityDbContext<User>
+    {
+        public ApplicationContext(DbContextOptions<ApplicationContext> options)
+            : base(options)
+        {
+            Database.EnsureCreated();
+        }
+
+        //public DbSet<User> AppUsers { get; set; }
+    }
+}
