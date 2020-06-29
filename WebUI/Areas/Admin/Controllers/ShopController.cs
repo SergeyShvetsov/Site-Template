12513224b17b50cb87524ce82c6f5a97b60f544b
sergey.shvetsov <svs1966uz@gmail.com>
2020-06-29 10:44:42 +0500

    Lesson  9-10

--- WebUI/Areas/Admin/Controllers/ShopController.cs
+++ WebUI/Areas/Admin/Controllers/ShopController.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Data.Model;
+using Data.Model.Models;
 using Microsoft.AspNetCore.Mvc;
 using WebUI.Areas.Admin.Models;
 
@@ -22,6 +23,70 @@ namespace WebUI.Areas.Admin.Controllers
                 .ToList();
 
             return View(categories);
+        }
+
+        // Урок 9
+        [HttpPost]
+        public string AddNewCategory(string catName)
+        {
+            if (db.Categories.Any(x => x.Name == catName)) return "titletaken";
+            var dto = new CategoryDTO()
+            {
+                Name = catName,
+                Slug = catName.Replace(" ", "-").ToLower(),
+                Sorting = db.Categories.Any() ? db.Categories.Max(x => x.Sorting) + 1 : 1
+            };
+            db.Categories.Add(dto);
+            db.SaveChanges();
+
+            return dto.Id.ToString();
+
+        }
+        public IActionResult DeleteCategory(Guid id)
+        {
+            var item = db.Categories.Find(id);
+            if (item != null)
+            {
+                db.Categories.Remove(item);
+                db.SaveChanges();
+
+                TempData["SM"] = "You have deleted a category!";
+                return RedirectToAction("Categories");
+            }
+
+            return Content("The category does not exist.");
+        }
+
+        // Создаем метод сортировки
+        [HttpPost]
+        public void ReorderCategories(string ids)
+        {
+            var idsArray = ids.Replace("id_", "").Replace("[]=", "-").Split('&');
+            int count = 1; // Реализуем счетчик  
+
+            CategoryDTO dto;
+            // Устанавливаем сортировку для каждой страницы
+            foreach (var catId in idsArray)
+            {
+                var guid = new Guid(catId);
+                dto = db.Categories.Find(guid);
+                dto.Sorting = count;
+
+                db.SaveChanges();
+                count++;
+            }
+        }
+
+        [HttpPost]
+        public string RenameCategory(string newCatName, Guid id)
+        {
+            if (db.Categories.Any(x => x.Name == newCatName)) return "titletaken";
+            var dto = db.Categories.Find(id);
+            dto.Name = newCatName;
+            dto.Slug = newCatName.Replace(" ", "-").ToLower();
+            db.SaveChanges();
+
+            return "ok";
         }
     }
 }
---
+++ WebUI/wwwroot/js/PagesIndexView.js
@@ -0,0 +1,18 @@
+﻿$(function () {
+    /* Confirm Page deletion */
+    $("a.delete").click(function () {
+        if (!confirm("Confirm Page deletion")) return false;
+    });
+    /*---------------------------------------*/
+    /* Sorting script */
+    $("table#pages tbody").sortable({
+        items: "tr:not(.home)",
+        placeholder: "ui-state-highlight",
+        update: function () {
+            var ids = $("table#pages tbody").sortable("serialize");
+            var url = "/Admin/Pages/ReorderPages";
+
+            $.post(url, { ids });
+        }
+    });
+});
